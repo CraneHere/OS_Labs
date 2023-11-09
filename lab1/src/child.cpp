a1bcb08db10d846660992276d1ee67a72d298fab
KataNemu <danilzinovev333@gmail.com>
2023-11-09 17:16:25 +0300

    updated child 1 lab

--- lab1/src/child.cpp
+++ lab1/src/child.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <fstream>
 
+bool isVowel(char c) {
+    return c == 'e' ||
+           c == 'u' ||
+           c == 'i' ||
+           c == 'o' ||
+           c == 'a';
+}
+
+
 int main(int argc, char **argv) {
     if (argc < 2) {
         printf("No input file.\n");
@@ -11,8 +20,14 @@ int main(int argc, char **argv) {
     std::ofstream file(filename);
 
     for (std::string line; std::getline(std::cin, line); ) {
-        // Удаляем гласные из лайна
         line.push_back('\n');
+        if (line.length() > 0) {
+            for (size_t i = 0; i < line.length(); ++i) {
+                if (isVowel(tolower(line[i]))) {
+                    line.erase(i, 1);
+                }
+            }
+        }
         file << line;
     }
 
