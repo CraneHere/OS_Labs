4833897bf310dcc52c2326c04fb7ab812847e976
Crane <113766015+CraneHere@users.noreply.github.com>
2024-03-01 22:06:48 +0300

    updated

--- KP/cp/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Shy/Documents/OS_Labs/CP
-# Build directory: /Users/Shy/Documents/OS_Labs/CP/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("cp")
-subdirs("tests")
--- KP/cp/build/cp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Shy/Documents/OS_Labs/CP/cp
-# Build directory: /Users/Shy/Documents/OS_Labs/CP/build/cp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
