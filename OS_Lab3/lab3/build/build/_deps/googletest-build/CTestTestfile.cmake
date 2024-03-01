fed39d5da0d65c54fcfac61ed30bcaa3e99d1bd2
Crane <113766015+CraneHere@users.noreply.github.com>
2024-03-01 22:07:28 +0300

    Delete OS_Lab3/lab3/build/build directory

--- OS_Lab3/lab3/build/build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Shy/Documents/OS_Labs/OS_Lab3/lab3/build/build/_deps/googletest-src
-# Build directory: /Users/Shy/Documents/OS_Labs/OS_Lab3/lab3/build/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
